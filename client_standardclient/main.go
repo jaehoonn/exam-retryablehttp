aec5c33807fbf6c196039d8585e5c0f7882ae849
JaehoonKim <jaehoon@nexclipper.io>
2021-03-04 16:10:04 +0900

    retryablehttp 의 로그 hook

--- client_standardclient/main.go
+++ client_standardclient/main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"time"
 
+	"github.com/NexClipper/logger"
 	"github.com/hashicorp/go-retryablehttp"
+	"github.com/jaehoonn/exam-retryablehttp/common"
 )
 
 func main() {
+	loggerEnv := &common.LoggerEnv{
+		Level:      "debug",
+		LogPath:    "./log/klevr.log",
+		MaxSize:    1,
+		MaxBackups: 3,
+		MaxAge:     1,
+		Compress:   false,
+	}
+
+	common.InitLogger(loggerEnv)
+	logger.Debug("start")
 	client := retryablehttp.NewClient()
+	client.Logger = nil
+	client.RequestLogHook = func(l retryablehttp.Logger, req *http.Request, n int) {
+		logger.Debugf("[%s] %s", req.Method, req.URL.String())
+	}
+
 	client.RetryMax = 5
 	client.RetryWaitMin = time.Second * 3
 	standardClient := client.StandardClient()
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/jaehoonn/exam-retryablehttp
 
 go 1.15
 
-require github.com/hashicorp/go-retryablehttp v0.6.8
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/NexClipper/logger v1.1.6
+	github.com/hashicorp/go-cleanhttp v0.5.1
+	github.com/hashicorp/go-retryablehttp v0.6.8
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
